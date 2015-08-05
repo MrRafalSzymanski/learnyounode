52129a823ee367f1dea2ef2d989a46f5b5c9900c
Rafal Szymanski <mr.rafal.szymanski@outlook.com>
2015-08-05 15:17:16 +0200

    Added some comments tp the solution and a semicolon

--- 02.js
+++ 02.js
@@ -2,10 +2,12 @@
  * @author rafal
  */
 
+//Storing passed parameters (without the command and path)
 var args = process.argv.slice(2);
 
+//Using reduce to sum array elements converted to ints, additional checks for type would be nice
 var sum = args.reduce(function(prev, curr, idx, array){	
 	return parseInt(prev) + parseInt(curr);
-})
+});
 
 console.log(sum);
