9fe5efd366fe5a1cae4ecb94787d4cc6e6b6b60a
Rafal Szymanski <mr.rafal.szymanski@outlook.com>
2015-08-05 15:52:49 +0200

    Writing those is becoming bit tedious... So this is a solution for the 5th task "Filtered LS"

---
+++ 05.js
@@ -0,0 +1,21 @@
+/**
+ * @author rafal
+ */
+
+//Load I/O module
+var fs = require("fs");
+
+//Regular expression that checks is string ends with particular string
+var regexRule = new RegExp("." + process.argv[3] + "$");
+
+//Read file asynchronously
+fs.readdir(process.argv[2], function(err, files){
+	
+	//Calling anonymous function for each element of the array (all files in folder)	
+	files.forEach(function(element, index, array){
+		//Testing against matching regular expression
+		if(regexRule.test(element)){
+			console.log(element);
+		}	
+	});
+});
